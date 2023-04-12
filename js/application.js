0d2bd0588e5baee929c948d9697f490b98a05ec3
Kuznetsov Anton <ASKuznetsov@sberbank.ru>
2023-04-12 12:29:05 +0400

    add button

--- js/application.js
+++ js/application.js
@@ -2,3 +2,24 @@
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
+
+const button = document.getElementById('post-btn');
+
+button.addEventListener('click', async _ => {
+  try {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+                    get: (searchParams, prop) => searchParams.get(prop),
+    });
+    data = {"token": params.token, "message" : "молодец ты набрал 8 очков, а здесь бы мог быть лид магнит если бы я что то продавала"}
+    const response = await fetch('https://proxy-gpt.herokuapp.com/api/salesbot/message', {
+      method: 'post',
+      headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(data)
+    });
+  } catch(err) {
+    console.error(`Error: ${err}`);
+  }
+});
